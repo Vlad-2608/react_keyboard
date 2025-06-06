1004976735dac4be35803d699d011ab50567e489
unknown <vladhafarov@gmail.com>
2025-06-06 19:56:37 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,73 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+// interface State {
+//   pressedKey: string;
+// }
+
+// export class App extends React.Component<{}, State> {
+//   setState(_arg0: { pressedKey: string }) {
+//     throw new Error('Method not implemented.');
+//   }
+
+//   state: Readonly<State> = {
+//     pressedKey: 'Nothing was pressed yet',
+//   };
+
+//   handleKeyUp = (event: KeyboardEvent) => {
+//     this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
+//   };
+
+//   componentDidMount(): void {
+//     document.addEventListener('keyup', this.handleKeyUp);
+//   }
+
+//   componentWillUnmount(): void {
+//     document.removeEventListener('keyup', this.handleKeyUp);
+//   }
+
+//   render(): React.ReactNode {
+//     const { pressedKey } = this.state;
+
+//     return (
+//       <div className="App">
+//         <p className="App__message">{pressedKey}</p>
+//       </div>
+//     );
+//   }
+// }
+
+type State = {
+  pressedKey: string;
+};
+
+export class App extends React.Component {
+  state: State = {
+    pressedKey: '',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleClick);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleClick);
+  }
+
+  handleClick = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressedKey
+            ? `The last pressed key is [${pressedKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
